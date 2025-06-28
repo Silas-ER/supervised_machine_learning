0c363b21971be68063bf0351a8e5028119c43cea
Silas Eduardo <silasn1@live.com>
2025-06-28 18:56:38 -0300

    aba de EDA completa

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# supervised_machine_learning
+# Previsão de Atrasos de Voos Utilizando Aprendizado de Máquina Supervisionado
 Projeto da terceira unidade de aprendizado de máquina supervisionado - IMD3002
 
 passos: 
     virtualenv venv
     venv/Scripts/activate
+    pip install -r requirements.txt
     streamlit run main.py
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 import seaborn as sns
 import streamlit as st
 import os
@@ -150,7 +151,157 @@ with tab1:
         st.dataframe(info_df)
 
         st.write(f"Shape: {airports.shape}")
+
 with tab2:
-    pass
+    st.header("Análise Exploratória de Dados - EDA")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        # Atrasos na chegada
+        st.subheader("Distribuição de Atrasos na Chegada")
+        delayed_pct = flights['ARRIVAL_DELAY'].apply(lambda x: 1 if x > 15 else 0).mean() * 100
+        st.write(f"Aproximadamente {delayed_pct:.2f}% dos voos sofreram atrasos superiores a 15 minutos.")
+
+        fig, ax = plt.subplots()
+        sns.histplot(flights['ARRIVAL_DELAY'], bins=50, kde=True, ax=ax, color='blue')
+        ax.set_title('Distribuição dos Atrasos na Chegada (ARRIVAL_DELAY)')
+        ax.set_xlabel('Minutos de Atraso')
+        ax.set_ylabel('Frequência')
+        ax.yaxis.set_major_formatter(FuncFormatter(lambda x, _: f'{int(x):,}'))
+        st.pyplot(fig)
+
+        st.markdown(""" 
+            **Observações:**
+            - O gráfico mostra que a maior parte dos atrasos na chegada está concentrada perto de 0 minutos (ou seja, muitos voos são pontuais ou chegam apenas alguns minutos atrasados).
+            - A cauda do gráfico esticada para a direita indica haver voos com atrasos muito longos (ex.: voos que se atrasam por dezenas ou centenas de minutos).
+        """)
+
+    with col2:
+        # Atrasos na decolagem
+        st.subheader("Distribuição de Atrasos na Decolagem")
+        delayed_departure_pct = flights['DEPARTURE_DELAY'].apply(lambda x: 1 if x > 15 else 0).mean() * 100
+        st.write(f"Aproximadamente {delayed_departure_pct:.2f}% dos voos sofreram atrasos superiores a 15 minutos na decolagem.")
+
+        fig, ax = plt.subplots()
+        sns.histplot(flights['DEPARTURE_DELAY'], bins=50, kde=True, ax=ax, color='orange')
+        ax.set_title('Distribuição dos Atrasos na Decolagem (DEPARTURE_DELAY)')
+        ax.set_xlabel('Minutos de Atraso')
+        ax.set_ylabel('Frequência')
+        ax.yaxis.set_major_formatter(FuncFormatter(lambda x, _: f'{int(x):,}'))
+        st.pyplot(fig)
+
+        st.markdown("""
+            **Observações:**
+            - Há uma concentração de voos pontuais ou com pequenos atrasos ou adiantamentos (próximo de 0 minutos).
+            - Assim como na chegada, existe uma cauda longa à direita nos atrasos de decolagem, indicando que voos atrasados na partida podem ter impacto significativo.
+        """)
+    
+    # Como os atrasos na partida afetam os atrasos na chegada
+    st.subheader("Impacto do Atraso na Partida no Atraso na Chegada")
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.scatterplot(x=flights['DEPARTURE_DELAY'], y=flights['ARRIVAL_DELAY'], alpha=0.5, ax=ax)
+    ax.set_title('Relação entre Atraso na Partida e na Chegada')
+    ax.set_xlabel('Atraso na Partida (minutos)')
+    ax.set_ylabel('Atraso na Chegada (minutos)')
+    st.pyplot(fig)
+
+    st.markdown("""
+        **Observações:**
+        - Quanto maior o atraso na partida, maior o atraso na chegada. A relação é quase linear, ou seja, se um voo atrasa 100 minutos na partida, ele tende a atrasar cerca de 100 minutos na chegada.
+        - Há pouca ou nenhuma recuperação de tempo significativo: Os pontos não se desviam muito da linha diagonal, o que indica que, em geral, os voos não conseguem compensar grandes atrasos que ocorrem na partida.
+        - Atrasos na partida são os principais impulsionadores dos atrasos na chegada. Raramente você vê um ponto com um grande atraso na partida e um pequeno atraso na chegada (o que significaria que o voo recuperou muito tempo).
+    """)
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        # Atrasos por cia aerea
+        st.subheader("Taxa de Atrasos por Companhia Aérea")
+        airline_delays = flights.groupby('AIRLINE')['ARRIVAL_DELAY'].mean()
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+        airline_delays.sort_values().plot(kind='bar', ax=ax, color='skyblue')
+        ax.set_title('Média de Atrasos por Companhia Aérea')
+        ax.set_xlabel('Companhia Aérea')
+        ax.set_ylabel('Atraso Médio na Chegada (minutos)')
+        st.pyplot(fig)
+
+    with col2:
+        # Atrasos por aeroportos
+        st.subheader("Top 10 Aeroportos com Mais Atrasos Médios")
+        airport_delays = flights.groupby('ORIGIN_AIRPORT')['ARRIVAL_DELAY'].mean().sort_values(ascending=False).head(10)
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+        airport_delays.plot(kind='bar', ax=ax, color='red')
+        ax.set_title('Atraso Médio na Chegada por Aeroporto de Origem')
+        ax.set_xlabel('Aeroporto')
+        ax.set_ylabel('Atraso Médio (minutos)')
+        st.pyplot(fig)
+
+    with col1:
+        # Atrasos por dia da semana
+        st.subheader("Atrasos por Dia da Semana")
+        flights['DAY_OF_WEEK'] = flights['DAY_OF_WEEK'].map({1: 'Segunda', 2: 'Terça', 3: 'Quarta', 4: 'Quinta',
+                                                            5: 'Sexta', 6: 'Sábado', 7: 'Domingo'})
+
+        week_day_delays = flights.groupby('DAY_OF_WEEK')['ARRIVAL_DELAY'].mean()
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+        week_day_delays.plot(kind='bar', ax=ax, color='green')
+        ax.set_title('Média de Atraso por Dia da Semana')
+        ax.set_xlabel('Dia da Semana')
+        ax.set_ylabel('Atraso Médio (minutos)')
+        st.pyplot(fig)
+    
+    with col2:
+        # Atrasos por hora do dia
+        st.subheader("Atrasos por Hora do Dia")
+
+        
+        flights['HOUR'] = flights['SCHEDULED_DEPARTURE'] // 100
+        hourly_delays = flights.groupby('HOUR')['ARRIVAL_DELAY'].mean()
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+        hourly_delays.plot(kind='line', ax=ax, marker='o')
+        ax.set_title('Atrasos Médios por Hora do Dia')
+        ax.set_xlabel('Hora do Dia')
+        ax.set_ylabel('Atraso Médio (minutos)')
+        st.pyplot(fig)
+
+    # Impacto da distancia nos atrasos
+    st.subheader("Impacto da Distância no Atraso")
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.scatterplot(x=flights['DISTANCE'], y=flights['ARRIVAL_DELAY'], alpha=0.6, ax=ax)
+    ax.set_title('Impacto da Distância no Atraso na Chegada')
+    ax.set_xlabel('Distância (milhas)')
+    ax.set_ylabel('Atraso na Chegada (minutos)')
+    st.pyplot(fig)
+
+    st.markdown("""
+        **Observações:**
+        - Há uma densa concentração de pontos nas distâncias menores (até aproximadamente 2500-3000 milhas). Isso sugere que a maioria dos voos na base de dados é de curta a média distância.
+        - Para todas as faixas de distância, observamos que os atrasos na chegada variam de 0 (ou muito próximos de 0) até valores bastante altos (por volta de 1500-2000 minutos). Isso significa que voos curtos, médios e longos podem sofrer atrasos significativos.
+        - Não há uma linha ou curva óbvia que mostre que voos mais longos têm sistematicamente mais (ou menos) atrasos do que voos mais curtos, ou vice-versa. Os pontos estão espalhados verticalmente em todas as faixas de distância.
+        - Este gráfico reforça a ideia de que os atrasos são provavelmente causados por outros fatores, como: Atrasos na partida (como vimos no gráfico anterior), Problemas operacionais no aeroporto de partida ou chegada, Condições meteorológicas, Problemas mecânicos da aeronave, Congestionamento do espaço aéreo.
+    """)
+
+    # Analise de correlações
+    st.subheader("Correlação entre Variáveis Contínuas")
+    fig, ax = plt.subplots(figsize=(12, 8))
+    sns.heatmap(flights[['ARRIVAL_DELAY', 'DEPARTURE_DELAY', 'DISTANCE', 'TAXI_OUT', 'AIR_TIME']].corr(), annot=True, cmap='coolwarm', ax=ax)
+    ax.set_title('Mapa de Correlação')
+    st.pyplot(fig)
+
+    st.markdown("""
+        **Maiores Correlações:**
+        - ARRIVAL_DELAY vs. DEPARTURE_DELAY (Atraso na Chegada vs. Atraso na Partida):
+            - Quanto maior o atraso na partida, maior tende a ser o atraso na chegada.
+        - DISTANCE vs. AIR_TIME (Distância vs. Tempo de Voo):
+            - É intuitivo: quanto maior a distância do voo, maior o tempo de voo.
+        - TAXI_OUT vs. ARRIVAL_DELAY (Tempo de Taxiamento na Saída vs. Atraso na Chegada):
+            - Há uma correlação positiva, mas relativamente fraca. Um tempo de taxiamento maior pode contribuir um pouco para o atraso na chegada, mas não é um fator tão dominante quanto o atraso na partida.
+    """)
+
 with tab3:
     pass
